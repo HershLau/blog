3d598c120c8fbf6daed46b5a3907a1546bce502f
hersh <neu_lhx@163.com>
2017-06-28 08:58:24 +0800

    创建项目

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# blog
-基于vue2.0+node.js+mongodb的个人博客项目
+# vue+node 实现的一套简易博客系统,包括博客前端展示页和博客后台管理页
+
+17-04-27更新如下，调整博客前端展示页面样式，支持响应式，添加了评论功能，UI参照[美团点评技术团队](http://tech.meituan.com/)的，莫怪
+
+> 简要技术栈：
+>
+> vue2.0+vue-router+vue-resource
+>
+> node+express+mongodb
+>
+> 后台管理页的编辑器用的simplemde，支持markdown语法
+>
+> markdown解析部分使用了marked这个库，语法高亮用highlight
+
+### [博客在线地址](https://weiweiblog.herokuapp.com)（仅供临时预览）
+ 
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/blog01.png)
+---
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/blog02.png)
+---
+
+### [博客后台管理页在线地址](https://weiweiblog.herokuapp.com/admin)（仅供临时预览）
+
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/admin01.png)
+---
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/admin02.png)
+---
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/admin3.png)
+---
+![](https://github.com/linguowei/myblog/blob/master/PreviewImg/admin4.png)
+---
+### 本地查看方法
+
+``` bash
+# git clone https://github.com/linguowei/myblog.git
+# cd myblog
+# npm install
+# node app.js
+# localhost:9000
+# localhost:9000/admin
+```
+### License
+[MIT](https://www.oschina.net/question/54100_9455)
---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+    <div id="app">
+        <el-row :gutter="0">
+            <el-col :xs="24" :sm="4" :md="4" :lg="4">
+                 <div class="nav-bar">
+                    <div class="nav-bar-inner">
+                        <span>weiwei的日常</span>
+                        <div>
+                            <i class="el-icon-menu" @click="navBar"></i>
+                        </div>
+                    </div>
+                    <div class="mobile-nav-bar" v-show="isShowMobileNavBar">
+                        <ul>
+                            <li @click="latestArticles" v-bind:class="{current: $route.path== '/'}">
+                                <span>最新文章</span>
+                            </li>
+                            <li @click="archives" v-bind:class="{current: $route.path== '/archives'}">
+                                <span style="border-left: 1px solid #ddd;">文章归档</span>
+                            </li>
+                            <li @click="tag" v-bind:class="{current: $route.path== '/tag'}">
+                                <span>文章标签</span>
+                            </li>
+                            <li @click="about" v-bind:class="{current: $route.path== '/about'}">
+                                <span style="border-left: 1px solid #ddd;">关于我的</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="nav-bar-body">
+                        <ul>
+                            <li @click="latestArticles" v-bind:class="{borderRightActive: $route.path=='/'}">最新文章</li>
+                            <li @click="archives" v-bind:class="{borderRightActive: $route.path=='/archives'}">文章归档</li>
+                            <li @click="tag" v-bind:class="{borderRightActive: $route.path=='/tag'}">文章标签</li>
+                            <li @click="about" v-bind:class="{borderRightActive: $route.path=='/about'}">关于我的</li>
+                        </ul>
+                    </div>
+                    <div class="nav-bar-footer">
+                        <span>粤ICP备17022497号</span>
+                    </div>
+                </div>
+            </el-col>
+            <el-col :xs="24" :sm="20" :md="20" :lg="20">
+                <div class="main">
+                    <transition name="fade" mode="out-in">
+                        <!--<keep-alive include="latestArticles">-->
+                            <router-view></router-view>
+                        <!--</keep-alive>-->
+                        <!--<router-view v-if="$route.meta.keepAlive == false"></router-view>-->
+                    </transition>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            isShowMobileNavBar: false
+        }
+    },
+    directives: {
+
+    },
+    methods: {
+        latestArticles: function(){
+            this.$router.push('/')
+        },
+        archives: function(){
+            this.$router.push('archives')
+        },
+        tag: function(){
+            this.$router.push('tag')
+        },
+        about: function(){
+            this.$router.push('about')
+        },
+        navBar: function(){
+            let result = window.matchMedia('(min-width: 768px)')
+            if(result.matches)return;
+            if(this.isShowMobileNavBar){
+                this.isShowMobileNavBar = false
+                var navBar = document.querySelector('.nav-bar')
+                navBar.style.height = 8 + 'vh'
+                navBar.style.transition = '0.5s'
+                
+            }else{
+                this.isShowMobileNavBar = true
+                var navBar = document.querySelector('.nav-bar')
+                navBar.style.height = 20 + 'vh'
+                navBar.style.transition = '0.5s'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+@import './style/common.css';
+</style>
