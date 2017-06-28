df6d532f112a5007cdafb0f5150cd249ddc2b361
hersh <neu_lhx@163.com>
2017-06-28 14:08:49 +0800

    修改项目

--- server/db.js
+++ server/db.js
@@ -1,38 +1,33 @@
 var mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
-// mongoose.connect('mongodb://wei1:0987654321@ds161018.mlab.com:61018/weiwei')
-mongoose.connect('mongodb://localhost:27017/weiweiblog')
+mongoose.connect('mongodb://localhost:27017/blog')
 
 var userSchema = new mongoose.Schema({
-	name: String,
-	pwd: String,
+  name: String,
+  pwd: String,
+  individualitySignature: String,
+  introduce: String
 })
 
 var articleSchema = new mongoose.Schema({
-	title: String,
-	date: Date,
-	articleContent: String,
-	state: String,
-	label: String,
+  title: String,
+  date: Date,
+  articleContent: String,
+  state: String,
+  label: String,
+  uid: String,
+  tids: [String]
 })
 
 var tagSchema = new mongoose.Schema({
-    tagName: String,
-    tagNumber: Number,
-})
-
-var personalInformationSchema = new mongoose.Schema({
-    name: String,
-    individualitySignature: String,
-    introduce: String,
+  tagName: String
 })
 
 var Models = {
-	User: mongoose.model('User', userSchema),
-	Article: mongoose.model('Article', articleSchema),
-	TagList: mongoose.model('TagList', tagSchema),
-    PersonalInformation: mongoose.model('PersonalInformation', personalInformationSchema),
+  User: mongoose.model('User', userSchema),
+  Article: mongoose.model('Article', articleSchema),
+  TagList: mongoose.model('TagList', tagSchema)
 }
 
 module.exports = Models
